8d8fff0b6ea62ad8810b2bc6661562d69b1d5084
Marcello Pato <marcellocorreia@gmail.com>
2023-05-17 17:59:18 -0300

    Correcting var names

--- criar_security_group.sh
+++ criar_security_group.sh
@@ -5,7 +5,7 @@ security_group_name="executiva-local"
 
 # Cria o grupo de segurança
 echo "Criando o grupo de segurança..."
-group_id=$(aws ec2 create-security-group --group-name "$security_group_name" --description "Grupo de segurança para acesso à porta 80" --output text)
+group_id=$(aws ec2 create-security-group --group-name $security_group_name --description "Grupo de segurança para acesso à porta 80" --output text)
 echo "Grupo de segurança criado com sucesso! ID do grupo: $group_id"
 
 # Autoriza o tráfego na porta 80
